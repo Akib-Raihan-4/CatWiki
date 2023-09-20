216b98d5c5d2a8c84039e06fe9d5a272b9b19d3a
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-20 15:45:59 -0400

    can count the number of times a breed is searched and stored in firebase

--- components/search .js
+++ components/search .js
@@ -2,20 +2,22 @@
 import React, { useState, useEffect } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import axios from 'axios';
-import './headers.css'
-
+import './headers.css';
 
 export const Search = () => {
   const [inputValue, setInputValue] = useState('');
   const [breedSuggestions, setBreedSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  
 
   useEffect(() => {
     async function fetchBreeds() {
       try {
         const res = await axios.get("https://api.thecatapi.com/v1/breeds");
-        const breeds = res.data.map((breed) => breed.name);
+        const breeds = res.data;
         setBreedSuggestions(breeds);
+        setFilteredSuggestions(breeds); // Initialize filtered suggestions with all breeds
       } catch (err) {
         console.log({ Message: "Could not fetch breed data" });
       }
@@ -24,20 +26,70 @@ export const Search = () => {
     fetchBreeds();
   }, []);
 
-  const filteredBreeds = breedSuggestions.filter((breed) =>
-    breed.toLowerCase().includes(inputValue.toLowerCase())
-  );
-
-  const handleSuggestionClick = (suggestion) => {
-    
-    setInputValue(suggestion);
+  const handleSuggestionClick = async (suggestion) => {
+    setInputValue(suggestion.name);
+  
+    // Get the breed ID from the suggestion
+    const breedId = suggestion.id;
+  
+    if (suggestion.name && breedId) {
+      const databaseUrl = `https://catbreed-96dbf-default-rtdb.asia-southeast1.firebasedatabase.app/breedData/${breedId}.json`;
+  
+      try {
+        // Fetch the existing data from the database
+        const existingDataResponse = await fetch(databaseUrl);
+        if (existingDataResponse.ok) {
+          const existingData = await existingDataResponse.json();
+  
+          // Get the current count or default to 0
+          const currentCount = existingData?.count || 0;
+  
+          // Update the count
+          const updatedCount = currentCount + 1;
+  
+          // Send a PATCH request to update the count in the database
+          const res = await fetch(databaseUrl, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              count: updatedCount, // Update the count
+            }),
+          });
+  
+          if (res.ok) {
+            // Data updated successfully
+            // You can handle success here
+          } else {
+            // Handle errors here
+            console.error("Failed to update data");
+          }
+        } else {
+          // Handle errors here
+          console.error("Failed to fetch existing data");
+        }
+      } catch (error) {
+        console.error("Error updating data:", error);
+      }
+    }
+  
     setShowSuggestions(false);
   };
+  
+  
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
-   
+
+    // Filter the suggestions based on the input value
+    const filtered = breedSuggestions.filter((suggestion) =>
+      suggestion.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setFilteredSuggestions(filtered); // Update the filtered suggestions
+  
     setShowSuggestions(!!value);
   };
 
@@ -58,16 +110,16 @@ export const Search = () => {
         </div>
       </div>
 
-      {showSuggestions && filteredBreeds.length > 0 && (
+      {showSuggestions && filteredSuggestions.length > 0 && (
         <div className='mt-24 bg-white text-black w-[80%] max-h-[200px] h-fit search rounded-[30px]'>
           <ul className="suggestion-list">
-            {filteredBreeds.map((suggestion, index) => (
+            {filteredSuggestions.map((suggestion, index) => (
               <li
                 key={index}
                 onClick={() => handleSuggestionClick(suggestion)}
                 className="cursor-pointer p-2 hover:bg-gray-200"
               >
-                {suggestion}
+                {suggestion.name}
               </li>
             ))}
           </ul>
