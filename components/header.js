4d57a5154228594a55227c8cff14d3c8c9d71143
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-19 22:37:17 -0400

    fetching data from cat api and display the image

--- components/header.js
+++ components/header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import './headers.css'
 import  {Search}  from './search '
+import  MostSearchedBreed  from './mostSearchedBreed'
+import {FaLongArrowAltRight} from 'react-icons/fa'
 
 export default function Header() {
   return (
@@ -22,11 +24,12 @@ export default function Header() {
             <div className='bg-[#4d270c] rounded-[77px] w-[59px] h-[3px] ml-20'></div>
             <div className='mt-8 ml-20 flex justify-between w-[1200px]'>
                 <h1 className="[font-family:'Montserrat-Bold',Helvetica] font-bold text-[#291506] text-[48px] tracking-[0] leading-[normal] max-w-[42%]">66+ Breeed For you to discover</h1>
-                <a className='flex items-end'>
-                    <p className="[font-family:'Montserrat-Bold',Helvetica] font-semibold text-[#29150799] text-[18px] ">SEE MORE</p>
+                <a className='flex items-end justify-evenly ' href='#'>
+                    <p className="[font-family:'Montserrat-Bold',Helvetica] font-semibold text-[#29150799] text-[18px] pr-2  ">SEE MORE </p>
+                    <FaLongArrowAltRight color='#29150799' size={28}  />
                 </a>
             </div>
-            
+            <MostSearchedBreed/>
         </div>
     </div>
   )
---
+++ components/mostSearchedBreed.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import axios from 'axios'
+import Image from 'next/image'
+
+async function catData() {
+  try{
+    const res = await axios.get("https://api.thecatapi.com/v1/images/search?limit=4&api_key=live_AvpOFJQDCN1uusCORCQivt7Yl3ErSTJ5Sg5zRXbX26MHHUEPoN4KZ2KGaEV14q1C")
+    // console.log(res)
+    return res.data
+  }catch(err){
+    console.log({Message:"Could not fetch data"})
+  }
+}
+
+export default async function MostSearchedBreed() {
+  const catImage = await catData()
+  return (
+    <div className='flex justify-between'>
+        {catImage.map((cat,index)=>(
+          <img src={cat.url} key={index} className='w-[220px] h-[220px] rounded-[20px]' />
+        ))}
+    </div>
+  )
+}
