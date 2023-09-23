971fd6320d10e2f4ba894d46c97acb619c035b38
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-23 16:12:58 -0400

    breeds page is done

--- app/breeds/components/catBreed.js
+++ app/breeds/components/catBreed.js
@@ -1,30 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Ratings } from './ratings';
-
-// const Ratings = ({ rating }) => {
-//   const filledStars = Math.floor(rating);
-
-
-//   const stars = [];
-
-//   for (let i = 0; i < filledStars; i++) {
-//     stars.push(
-//       <div key={i} className="w-[60px] h-[12px] bg-[#544439] rounded-[8px] "/>
-//     );
-//   }
-
-  
-
-//   while (stars.length < 5) {
-//     stars.push(
-//       <div key={stars.length} className="w-[60px] h-[12px] bg-[#e0e0e0] rounded-[8px] "/>
-//     );
-//   }
-
-//   return <div className="flex gap-7">{stars}</div>;
-// };
-
-
+import { OtherPhoto } from './otherPhoto';
 
 
 export const CatBreed = ({ breedID }) => {
@@ -55,78 +31,79 @@ export const CatBreed = ({ breedID }) => {
   return (
     <div className='mt-10 w-[1380px] mx-auto'>
       {catInfo.length > 0 && (
-        
-        <div className='flex'>
-          {console.log(catInfo)}
-          <div className='relative mr-40'>
-            <img
-              key={catInfo[0].id}
-              src={catInfo[0].url}
-              alt={`Cat Image ${catInfo.id}`}
-              className="w-[371px] h-[371px] object-cover rounded-[25px] relative shadow-[10px_10px_10px_5px_rgba(0,0,0,0.5)]"
-            />
-            {/* <div className='w-[84px] h-[315px] bg-[#dec68a] rounded-[14px] -z-10 absolute top-[25px] -left-3'/> */}
+        <div>
+          <div className='flex'>
+            {/* {console.log(catInfo.length)} */}
+            <div className='relative mr-40'>
+              <img
+                key={catInfo[0].id}
+                src={catInfo[0].url}
+                alt={`Cat Image ${catInfo.id}`}
+                className="w-[371px] h-[371px] rounded-[25px] relative shadow-[10px_10px_10px_5px_rgba(0,0,0,0.5)]"
+              />
+              {/* <div className='w-[84px] h-[315px] bg-[#dec68a] rounded-[14px] -z-10 absolute top-[25px] -left-3'/> */}
+            </div>
+            <div>
+              <h1 className="[font-family:'Montserrat-SemiBold',Helvetica] font-semibold text-[#291507] text-[36px]">
+                {catInfo[0].breeds[0].name}
+              </h1>
+              <p className="w-[601px] [font-family:'Montserrat-Medium',Helvetica] font-medium text-[#291507] text-[18px] my-6">{catInfo[0].breeds[0].description}</p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6">
+                <span className="font-bold">Temperament: </span>
+                <span className="[font-family:'Montserrat-Medium',Helvetica] font-medium">
+                  {catInfo[0].breeds[0].temperament}
+                </span>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6">
+                <span className="font-bold">Origin: </span>
+                <span className="[font-family:'Montserrat-Medium',Helvetica] font-medium">
+                  {catInfo[0].breeds[0].origin}
+                </span>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6">
+                <span className="font-bold">Life Span: </span>
+                <span className="[font-family:'Montserrat-Medium',Helvetica] font-medium">
+                  {catInfo[0].breeds[0].life_span} years
+                </span>
+              </p>
+
+              {/* Bars are here */}
+
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Adaptibility: </span>
+                <Ratings rating={catInfo[0].breeds[0].adaptability}/>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Affection level: </span>
+                <Ratings rating={catInfo[0].breeds[0].affection_level}/>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Child Friendly: </span>
+                <Ratings rating={catInfo[0].breeds[0].child_friendly}/>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Grooming: </span>
+                <Ratings rating={catInfo[0].breeds[0].grooming}/>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Intelligent: </span>
+                <Ratings rating={catInfo[0].breeds[0].intelligence}/>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Health issues: </span>
+                <Ratings rating={catInfo[0].breeds[0].health_issues}/>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Social needs: </span>
+                <Ratings rating={catInfo[0].breeds[0].social_needs}/>
+              </p>
+              <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
+                <span className="font-bold block w-[180px]">Stranger friendly: </span>
+                <Ratings rating={catInfo[0].breeds[0].stranger_friendly}/>
+              </p>
+            </div>
           </div>
-          <div>
-            <h1 className="[font-family:'Montserrat-SemiBold',Helvetica] font-semibold text-[#291507] text-[36px]">
-              {catInfo[0].breeds[0].name}
-            </h1>
-            <p className="w-[601px] [font-family:'Montserrat-Medium',Helvetica] font-medium text-[#291507] text-[18px] my-6">{catInfo[0].breeds[0].description}</p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6">
-              <span className="font-bold">Temperament: </span>
-              <span className="[font-family:'Montserrat-Medium',Helvetica] font-medium">
-                {catInfo[0].breeds[0].temperament}
-              </span>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6">
-              <span className="font-bold">Origin: </span>
-              <span className="[font-family:'Montserrat-Medium',Helvetica] font-medium">
-                {catInfo[0].breeds[0].origin}
-              </span>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6">
-              <span className="font-bold">Life Span: </span>
-              <span className="[font-family:'Montserrat-Medium',Helvetica] font-medium">
-                {catInfo[0].breeds[0].life_span} years
-              </span>
-            </p>
-
-            {/* Bars are here */}
-
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Adaptibility: </span>
-              <Ratings rating={catInfo[0].breeds[0].adaptability}/>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Affection level: </span>
-              <Ratings rating={catInfo[0].breeds[0].affection_level}/>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Child Friendly: </span>
-              <Ratings rating={catInfo[0].breeds[0].child_friendly}/>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Grooming: </span>
-              <Ratings rating={catInfo[0].breeds[0].grooming}/>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Intelligent: </span>
-              <Ratings rating={catInfo[0].breeds[0].intelligence}/>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Health issues: </span>
-              <Ratings rating={catInfo[0].breeds[0].health_issues}/>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Social needs: </span>
-              <Ratings rating={catInfo[0].breeds[0].social_needs}/>
-            </p>
-            <p className="[font-family:'Montserrat-Bold',Helvetica] font-normal text-black text-[16px] mb-6 flex">
-              <span className="font-bold block w-[180px]">Stranger friendly: </span>
-              <Ratings rating={catInfo[0].breeds[0].stranger_friendly}/>
-            </p>
-
-          </div>
+          <OtherPhoto catInfos={catInfo}/>
         </div>
       )}
       
--- app/breeds/components/otherPhoto.js
+++ app/breeds/components/otherPhoto.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export const OtherPhoto = ({catInfos}) => {
-  return (
-    <div>OtherPhoto</div>
-  )
+    const images = []
+
+    for(let i = 1; i< catInfos.length; i++){
+        images.push(
+            <img src={catInfos[i].url} key={i} className="w-[345px] h-[278px] object-cover rounded-[25px] shadow-[10px_10px_10px_5px_rgba(0,0,0,0.5)]"/>
+        )
+    }
+
+    return(
+        <div className='mt-20 mb-32'>
+            <h1 className="[font-family:'Montserrat-SemiBold',Helvetica] font-semibold text-[#291507] text-[36px] mb-10">Other photos</h1>
+            <div className="grid grid-cols-4 gap-10 ">
+                {images}
+            </div>
+        </div>
+    )
 }
