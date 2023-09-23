1fddc48e58bea1a8d18078d33c0c809492d144b8
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-22 23:35:11 -0400

    can read the id in different page

--- app/breeds/page.jsx
+++ app/breeds/page.jsx
@@ -1,7 +1,11 @@
+"use client"
 import React from 'react'
-
+import { useSearchParams } from 'next/navigation'
 
 const Breeds = () => {
+  const searchParams = useSearchParams()
+  const search = searchParams.get('id')
+  console.log(search)
   return (
     <div>
         <h1>Breeds</h1>
