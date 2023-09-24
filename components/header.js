116bb768e2a61240bc5528095c56486c7f3de200
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-23 21:49:21 -0400

    header mobile view

--- components/header.js
+++ components/header.js
@@ -19,13 +19,13 @@ export default function Header() {
             </div>
         </div>
         <div className='bg-[#e4e4dc] rounded-b-[35px] '>
-            <p className="[font-family:'Montserrat-Medium',Helvetica] font-medium text-[#291507] text-[18px] pt-8 pl-20">Most Searched Breeds</p>
-            <div className='bg-[#4d270c] rounded-[77px] w-[59px] h-[3px] ml-20'></div>
-            <div className='mt-8 ml-20 flex justify-between w-[1200px]'>
-                <h1 className="[font-family:'Montserrat-Bold',Helvetica] font-bold text-[#291506] text-[48px] tracking-[0] leading-[normal] max-w-[42%]">66+ Breeed For you to discover</h1>
-                <a className='flex items-end justify-evenly ' href='/topBreeds'>
-                    <p className="[font-family:'Montserrat-Bold',Helvetica] font-semibold text-[#29150799] text-[18px] pr-2  ">SEE MORE </p>
-                    <FaLongArrowAltRight color='#29150799' size={28}  />
+            <p className="[font-family:'Montserrat-Medium',Helvetica] font-medium text-[#291507] sm:text-[18px] text-[12px] pt-8 sm:pl-20 pl-5">Most Searched Breeds</p>
+            <div className='bg-[#4d270c] rounded-[77px] sm:w-[59px] w-[37px] h-[3px] sm:ml-20 ml-5'/>
+            <div className='mt-8 sm:ml-20 ml-5 flex justify-between sm:w-[1200px]'>
+                <h1 className="[font-family:'Montserrat-Bold',Helvetica] font-bold text-[#291506] sm:text-[48px] text-[18px] tracking-[0] leading-[normal] sm:w-[537px] w-[185px] ">66+ Breeed For you to discover</h1>
+                <a className='flex items-end justify-evenly pr-2' href='/topBreeds'>
+                    <p className="[font-family:'Montserrat-Bold',Helvetica] font-semibold text-[#29150799] sm:text-[18px] text-[14px] pr-2  ">SEE MORE </p>
+                    <FaLongArrowAltRight color='#29150799' className='icon'  />
                 </a>
             </div>
             <MostSearchedBreed/>
