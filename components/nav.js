290a18d046ce97321d00e005be52feece3235f14
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-24 02:13:13 -0400

    logo home link added amd also link of whyShould

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 export const Nav = () => {
   return (
     <div className='sm:w-[1440px] mx-auto w-[95%] '>
+      <Link
+        href={"/"}
+      >
         <Image src="CatwikiLogo.svg" alt='catLogo' width={140} height={100} className='py-[20px] sm:w-[140px] sm:h-[100px] w-[100px] h-[80px]'/>
+      </Link>
     </div>
   )
 }
